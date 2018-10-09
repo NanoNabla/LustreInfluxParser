062d3d85b75c0d6d0eb3fda9db94dc930a1e484b
NanoNabla <43477372+NanoNabla@users.noreply.github.com>
2018-10-09 16:23:57 +0200

    fix overwriting cvs on different databases

--- file_writer.py
+++ file_writer.py
@@ -7,5 +7,5 @@ def write_csv(jobid, dataset):
     for db in dataset:
         for group in db:
             for measurement in group['measurements']:
-                np.savetxt(jobid + '_' + group['name'] + '_' + measurement['name'] + '.csv', measurement['value'],
+                np.savetxt(jobid + '_' + group['database'] + '_' + group['name'] + '_' + measurement['name'] + '.csv', measurement['value'],
                            delimiter=",")
